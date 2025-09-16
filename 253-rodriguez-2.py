f4db78f8e7959b02659d5ed13f66cd201e99d000
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-09-16 09:30:25 -0400

    9.11 lecture python added with code

--- 253-rodriguez-2.py
+++ 253-rodriguez-2.py
@@ -5,7 +5,7 @@ A = {0, 1, 2, 3, 5, 10, 12}
 print("The Set for A is ", A)
 B = {2, 4, 6, 8, 10, 12, 14, 16}
 print("The Set for B is ", B)
-print("The set for A or B is ",A | B)
+print("The set for A or B is ", A | B)
 print ("The set for A and B is ", A & B)
-print ("The set for A minus B is", A - B)
+print ("The set for A minus B is ", A - B)
 print ("The set for B minus A is ", B - A)
