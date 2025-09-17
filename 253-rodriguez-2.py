1271297637c86365d7c6b151fc795dafde27ce23
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-09-17 12:48:03 -0400

    9.11 lecture python added with code

--- 253-rodriguez-2.py
+++ 253-rodriguez-2.py
@@ -1,14 +1,14 @@
 print("P2: Sets")
 
 print("Question 1")
-A = {0, 1, 2, 3, 5, 10, 12}
+A = {0, 1, 2, 3, 5, 10, 12}   # creating a set A
 print("The Set for A is", A)
-B = {2, 4, 6, 8, 10, 12, 14, 16}
+B = {2, 4, 6, 8, 10, 12, 14, 16}   # creating a set B
 print("The Set for B is", B)
-print("The set for A or B is", A | B)
-print ("The set for A and B is", A & B)
-print ("The set for A minus B is", A - B)
-print ("The set for B minus A is", B - A)
+print("The set for A or B is", A | B)  # set A or set B
+print ("The set for A and B is", A & B)   # set A and set B
+print ("The set for A minus B is", A - B)   # set A minus B
+print ("The set for B minus A is", B - A)   # set B minus A
 
 print("Question 2")
 E = {2*n for n in range(0,51)}
