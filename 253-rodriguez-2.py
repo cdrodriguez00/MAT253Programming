b13244f9dad9f9b864872d628c3a07b0e71fb31f
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-09-18 19:19:01 -0400

    9.11 lecture python added with code

--- 253-rodriguez-2.py
+++ 253-rodriguez-2.py
@@ -10,15 +10,15 @@ print("The set for A and B is", A & B)   # set A and set B
 print("The set for A minus B is", A - B)   # set A minus B
 print("The set for B minus A is", B - A)   # set B minus A
 
-print("Question 2")
+print("\nQuestion 2")
 E = {2*n for n in range(0, 51)}   # creates a set E for all even numbers from 0 to 100
 print("The set E is", E)
 
-print("Question 3")
+print("\nQuestion 3")
 F = {n for n in range(101) if n % 2 == 0}   # # creates a set F for all even numbers from 0 to 100
 print("The set F is", F)
 
-print("Question 4")
+print("\nQuestion 4")
 print("Checking if E is a subset of F and if F is a subset of E")
 print("Is set E equal to set F?")
 print(E == F)
