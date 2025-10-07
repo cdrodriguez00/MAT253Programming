8fd335ec00f8a5a1c7128fd688ea95b5040f8279
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-10-07 16:27:33 -0400

    9.11 lecture python added with code

--- 253-rodriguez-3.py
+++ 253-rodriguez-3.py
@@ -2,3 +2,20 @@ print("P3: Functions")
 
 print("Question 1")
 
+
+def sumupto(n):
+    """write doc string"""
+    total = 0
+    for i in range(1, n+1):
+        total += i  # total = total + i is also equivalent
+        return total
+
+
+def triangular(n):
+    """write doc string """
+    t = n*(n+1)//2  # // for integer division
+    return t
+
+
+for n in range(1, 101):
+    print('n = ', n, 'S_n = ', sumupto(n), "T_n = ", triangular(n))
