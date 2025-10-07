355d3a35f03e4bd40afdb0265b6aaabffbb62f04
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-10-07 02:01:26 -0400

    9.11 lecture python added with code

--- 10.2 Lecture.py
+++ 10.2 Lecture.py
@@ -12,3 +12,20 @@ def absolute_value(n):
     if n < 0:
         n = -n
         return n
+
+def area(width, height):
+    return width * height
+
+"""Return the volume of a box given a width, length, and height."""
+def volume(width, length, height):
+    v = length * width * height
+    return v
+
+# function of volume has been defined
+# values can now be put into defined function for output
+# can return outputs and print things
+# any variables inside the function is a local function only used to calculate function
+    # cannot be called outside the function
+
+# doc string can show what makes up function - also the help command on the function
+
