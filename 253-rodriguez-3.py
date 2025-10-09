ed6d666e1eb5eaca6a6b5284f0d6ed8f641880f1
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-10-08 22:07:51 -0400

    9.11 lecture python added with code

--- 253-rodriguez-3.py
+++ 253-rodriguez-3.py
@@ -18,15 +18,28 @@ def sumupto(n):
 
 def triangular(n):
     """
-    Calculates and returns the nth triangularr number
-    :param n:
-    :return:
+    Calculates and returns the nth triangular number n(n+1)//2
+
+    Args:
+        n: integer n
+
+    Returns:
+        The nth triangular integer
     """
     t = n*(n+1)//2  # // for integer division
     return t
 
 def sumpower(n, k):
-    """"write doc string"""
+    """"
+    Calculates and returns the sum of n powers over the range 1 to n+1.
+
+    Args:
+        n: an integer for the end range n + 1
+        k: an integer for the power of each i in range
+
+    Returns:
+        The sum of the k power of a range of numbers i from 1 to n+1
+    """
     total = 0
     for i in range(1, n+1):
         total += i**k  # total = total + i**k is also equivalent
