2351d1199eb5a5792789f782cf2447573287758d
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-12-02 23:38:37 -0500

    9.11 lecture python added with code

--- 253-rodriguez-4.py
+++ 253-rodriguez-4.py
@@ -6,11 +6,18 @@ def hailstone(n):
     """takes positive integer n as input and returns a list called hailstone sequence
     first value
     sequence terminates at 1
+    function(n) if n is even, n = n/2
+    if n is odd, n= 3n+1
     """
-    if n%2 = 0:
-        n/2
-    elif n%2 != 0:
-        3n+2
+    collatz = [n]
+
+    while n != 1:
+        if n%2 == 0:
+            n =  n//2
+        elif n%2 != 0:
+            n = 3*n+2
+
+    return collatz
 
 
 
