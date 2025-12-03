75b79d8b3437d3c4e07b5e4e8f66c5951aaebbe2
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-12-02 23:56:53 -0500

    9.11 lecture python added with code

--- 253-rodriguez-4.py
+++ 253-rodriguez-4.py
@@ -16,22 +16,32 @@ def hailstone(n):
         if n%2 == 0:
             n =  n//2
         elif n%2 != 0:
-            n = 3*n+2
-    collatz.append(n)
+            n = 3*n+1
+        collatz.append(n)  #adds to the list
+
     return collatz
 
 print("Problem 1")
 print("Compute the hailstone sequence for 1")
-hailstone(1)
+print(hailstone(1))
 
 print("Problem 2")
 print("Compute the hailstone sequence for 27")
-hailstone(27)
+print(hailstone(27))
 
 
 print("Problem 3")
-print("Which positive integer n <= 20,000 has the longest hailstone sequence? How long is the /n"
+print("Which positive integer n <= 20,000 has the longest hailstone sequence? How long is the \n"
       "hailstone sequence of this integer?")
 
+longest_length = 0
+longest_n = 1
 
+for n in range(1, 20001):
+    length = len(hailstone(n))
+    if length > longest_length:
+        longest_length = length
+        longest_n = n
 
+print(longest_n)
+print(longest_length)
