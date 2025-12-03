b108f60841a9bfc056e45c4617140082aa5d3380
cristianrodriguez <cristiandrodriguez74@outlook.com>
2025-12-02 23:21:17 -0500

    9.11 lecture python added with code

---
+++ 253-rodriguez-4.py
@@ -0,0 +1,10 @@
+import math
+
+print("P4: More Functions and Lists")
+
+def hailstone(n):
+    """takes positive integer n as input and returns a list called hailstone sequence
+    first value
+    sequence terminates at 1
+    """
+
